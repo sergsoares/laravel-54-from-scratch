d75fb8fbdc5471f33e8277a52fd90ee565a0f8b5
Sergio Soares <acad.sergio@gmail.com>
2017-11-20 18:52:30 -0300

    Nav create link

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <nav class="nav">
         <a class="nav-link active" href="#">Home</a>
-        <a class="nav-link" href="#">New features</a>
+        <a class="nav-link" href="/posts/create">Create</a>
         <a class="nav-link" href="#">Press</a>
         <a class="nav-link" href="#">New hires</a>
         <a class="nav-link" href="#">About</a>
@@ -11,7 +11,7 @@
         @if(Auth::check())
             <a class="nav-link ml-auto" href="#">{{ Auth::user()->name }}</a>
         @endif
-        
+
         </nav>
     </div>
 </div>
