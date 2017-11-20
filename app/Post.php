ef9ed95f61e57db92b59a3b3d00e86f5dacd0c9b
Sergio Soares <acad.sergio@gmail.com>
2017-11-20 18:12:32 -0300

    Organizing for PHP pattern

--- app/Post.php
+++ app/Post.php
@@ -8,15 +8,18 @@ class Post extends Model
 {
     public $fillable = ['title', 'body'];
 
-    public function comments() {
+    public function comments() 
+    {
         return $this->hasMany(Comment::class);
     }
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function addComment($body){
+    public function addComment($body)
+    {
         $this->comments()->create(compact('body'));
     }
     
