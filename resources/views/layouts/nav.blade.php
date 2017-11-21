3d047c5574f0d5ff0dc3a5636490e8de52ddf057
Sergio Soares <acad.sergio@gmail.com>
2017-11-20 21:04:59 -0300

    Login and Logout same layout

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -4,13 +4,15 @@
         <nav class="nav">
         <a class="nav-link active" href="#">Home</a>
         <a class="nav-link" href="/posts/create">Create</a>
-        <a class="nav-link" href="/login">Login</a>
+        
         <a class="nav-link" href="#">New hires</a>
         <a class="nav-link" href="#">About</a>
         
         @if(Auth::check())
             <a class="nav-link ml-auto" href="#">{{ Auth::user()->name }}</a>
             <a class="nav-link " href="/logout">Logout</a>
+        @else
+            <a class="nav-link ml-auto" href="/login">Login</a>
         @endif
 
         </nav>
