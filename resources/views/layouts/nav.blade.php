43969a45b49db814101980714351708288c33e44
Sergio Soares <acad.sergio@gmail.com>
2017-11-20 21:02:38 -0300

    Added logging in the nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -4,7 +4,7 @@
         <nav class="nav">
         <a class="nav-link active" href="#">Home</a>
         <a class="nav-link" href="/posts/create">Create</a>
-        <a class="nav-link" href="#">Press</a>
+        <a class="nav-link" href="/login">Login</a>
         <a class="nav-link" href="#">New hires</a>
         <a class="nav-link" href="#">About</a>
         
