faa3668a78a751c8132deaa4f5ee50f3c080eeca
Sergio Soares <acad.sergio@gmail.com>
2017-11-22 13:06:57 -0300

    Implemented tag by name in url

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,14 @@ Route::get('posts/{post}' , 'PostController@show');
 //Comment Routes
 Route::post('/posts/{post}/comments', 'CommentController@store');
 
+Route::get('/posts/tags/{tag}', 'TagController@index');
+
+//Register
 Route::post('/register', 'RegistrationController@store');
 Route::get('/register', 'RegistrationController@create');
 
-
 Route::get('/login', 'SessionController@create')->name('login');
 Route::post('/login', 'SessionController@store');
-
 Route::get('/logout', 'SessionController@destroy');
 
 // Auth::routes();
